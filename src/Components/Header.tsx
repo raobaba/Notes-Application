59c3cd95c87a4e1c7c7b935ff73bb0aec0c4999f
raobaba <shrikishunr7@gmail.com>
2023-03-30 01:25:11 +0530

    header and app updated

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,7 @@ import {AppBar,Toolbar,Typography} from "@mui/material";
 import { logo } from "../Constant/Constant";
 export const Header:React.FC=()=> {
   return (
-    <AppBar color="transparent">
+    <AppBar color="transparent" position="static">
        <Toolbar>
           <img src={logo} alt="logo" style={{width:30,marginRight:10}} />
           <Typography>Evernote</Typography>
